50a537e4bdad871610f41db0586ed8deafe3636f
kishan200 <sarvaiyakishank@gmail.com>
2020-09-14 12:55:06 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+//
+alert('hello world');
+console.error('this is error');
+console.warn('this is warning')
+
+//let ,const
+let age=30;
+
+
+console.log(age);
+
+const score =10;
+
+console.log(score);
